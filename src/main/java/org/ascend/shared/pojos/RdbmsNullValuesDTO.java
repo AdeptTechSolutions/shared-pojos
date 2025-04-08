0c2d32d1c332d2e6b5713b0b346b0aeceebf9c59
NasirAziz-Adept <121924718+NasirAziz-Adept@users.noreply.github.com>
2025-04-08 12:56:30 +0500

    rdbms signature change

--- src/main/java/org/ascend/shared/pojos/RdbmsNullValuesDTO.java
+++ src/main/java/org/ascend/shared/pojos/RdbmsNullValuesDTO.java
@@ -1,5 +1,7 @@
 package org.ascend.shared.pojos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,4 +17,8 @@ public class RdbmsNullValuesDTO extends SqlJobDTO {
    String dataSource;
    String dataSourceName;
    Boolean isSingleTableScript;
+
+//   @NotNull(message = "Database user is required")
+//   @Schema(example = "true", description = "Whatever to extract and save DNA of database as well")
+   private Boolean shouldExtractSignatureDna;
 }
