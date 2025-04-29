c97317894719d76ac21023c280521e9c651ce8f8
NasirAziz-Adept <121924718+NasirAziz-Adept@users.noreply.github.com>
2025-04-29 12:31:45 +0500

    rdbms fixes

--- src/main/java/org/ascend/shared/pojos/rdbms/DatabaseCredentialsModel.java
+++ src/main/java/org/ascend/shared/pojos/rdbms/DatabaseCredentialsModel.java
@@ -10,47 +10,47 @@ import lombok.Data;
 public class DatabaseCredentialsModel {
 
 	@Schema(example = "4", description = "Database Credentials ID")
-	private final Integer id;
+	private Integer id;
 
 	@NotNull(message = "User ID is required")
 	@Schema(example = "10", description = "ID of user associated with the saved database credentials")
-	private final Long userId;
+	private Long userId;
 
 	@NotNull(message = "Database type is required")
 	@Schema(example = "Postgresql", description = "Type of database")
-	private final DatabaseTypeEnum type;
+	private DatabaseTypeEnum type;
 
 	@NotBlank(message = "Host Name cannot be blank")
 	@Schema(example = "https://your-host.com", description = "Host of database")
-	private final String hostName;
+	private String hostName;
 
 	@NotBlank(message = "Database Name cannot be blank")
 	@Schema(example = "your_database", description = "Name of database")
-	private final String databaseName;
+	private String databaseName;
 
 	@NotBlank(message = "User Name cannot be blank")
 	@Schema(example = "root", description = "User of database")
-	private final String userName;
+	private String userName;
 
 	@NotBlank(message = "Password cannot be blank")
 	@Schema(example = "your_password", description = "Password of database")
-	private final String password;
+	private String password;
 
 	@Min(value = 1, message = "Port must be greater than 0")
 	@Schema(example = "3306", description = "Port of database")
-	private final int port;
+	private int port;
 
 	@NotBlank(message = "Project Name cannot be blank")
 	@Schema(example = "DB Project", description = "Project Name associated with the saved database credentials")
-	private final String projectName;
+	private String projectName;
 
 	@NotNull(message = "Project ID is required")
 	@Schema(example = "80", description = "Project ID associated with database credentials")
-	private final Integer projectId;
+	private Integer projectId;
 
 	@Schema(example = "your-sid", description = "Oracle SID")
-	private final String oracleSid;
+	private String oracleSid;
 
-	private final Date createdAt;
+	private Date createdAt;
 
 }
--- src/main/java/org/ascend/shared/pojos/rdbms/RegenerateErdRequest.java
+++ src/main/java/org/ascend/shared/pojos/rdbms/RegenerateErdRequest.java
@@ -4,40 +4,44 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegenerateErdRequest {
 
 	private Integer id;
 
 	@NotNull(message = "Database type is required")
 	@Schema(example = "Postgresql", description = "Type of database")
-	private final DatabaseTypeEnum type;
+	private DatabaseTypeEnum type;
 
 	@NotBlank(message = "Host Name cannot be blank")
 	@Schema(example = "https://your-host.com", description = "Host of database")
-	private final String hostName;
+	private String hostName;
 
 	@NotBlank(message = "Database Name cannot be blank")
 	@Schema(example = "your_database", description = "Name of database")
-	private final String databaseName;
+	private String databaseName;
 
 	@NotBlank(message = "User Name cannot be blank")
 	@Schema(example = "root", description = "User of database")
-	private final String userName;
+	private String userName;
 
 	@NotBlank(message = "Password cannot be blank")
 	@Schema(example = "your_password", description = "Password of database")
-	private final String password;
+	private String password;
 
 	@Min(value = 1, message = "Port must be greater than 0")
 	@Schema(example = "3306", description = "Port of database")
-	private final int port;
+	private int port;
 
 	@Schema(example = "your-sid", description = "Oracle SID")
-	private final String oracleSid;
+	private String oracleSid;
 
 }
